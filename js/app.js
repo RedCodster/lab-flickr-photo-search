0fb0912812d6ed86b8ef81462ac4d8ff559d2717
RedCodster <rob_fong00@yahoo.com>
2016-02-04 17:04:07 +0800

    basic requirements met

--- js/app.js
+++ js/app.js
@@ -1,44 +1,51 @@
 // YOUR CODE HERE
 $(document).ready(function () {
 
+  var srchQuery;
+  var srchMode;
+
   var bindSearchButton = function () {
-    $('#bindSearchButton').on('click', function (e) {
+    $('#search').on('click', function (e) {
       e.preventDefault();
-
-  var getPhotos = function () {
-  $.ajax({
-        url: "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-        data: {
-          format: 'json'
-        method: "GET"
-        success: function (response, status) {
-        }
-
-        },
-        dataType: 'jsonp',
-        error: // YOUR CALLBACK
-      });
+      console.log('Buttons are bound!');
+      srchQuery = $('input.form-control').val();
+      srchMode = $('selectpicker.form-control').val();
+      getPhotos(srchQuery, srchMode);
+    });
   };
 
+
+  var getPhotos = function (tags, mode) {
+    $.ajax({
+        url: "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+        method: "GET",
+        data: {
+          format: 'json',
+          tags: tags,
+          tagmode: mode
+        },
+        dataType: 'jsonp',
+        success: function (response, status) {
+          $('div#images').empty();
+          var images = response.items;
+          images.forEach(function (elem,index) {
+            showNewPic(elem, index);
+          });
+        },
+        error: function (response, status) {
+        }
+      });
+    };
+
   var init = function () {
-    getPhotos();
     bindSearchButton();
   };
 
+  var showNewPic = function (elem, index) {
+    var newPic = ('<div><img src="'+elem.media.m+'"></li>');
+    $('#images').prepend(newPic);
+  };
 
-// var getAllDonuts = function () {
-//     $.ajax({
-//       url: "http://api.doughnuts.ga/doughnuts",
-//       method: "GET",
-//       success: function (response, status) {
-//         response.forEach(function (elem) {
-//           createNewLi(elem.id, elem.flavor, elem.style);
-//         });
-//         bindButtons();
-//       },
-//       error: function (response, status) {
-//         console.log(response);
-//       }
-//     });
-//   };
-)}
+    init();
+
+});
